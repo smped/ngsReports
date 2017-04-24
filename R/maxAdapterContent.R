969dcf1e321ca900a3822e57a6bd889d959de68a
Steve Pederson <stephen.pederson@adelaide.edu.au>
2017-04-24 12:46:03 +0930

    Added maxAdapterContent

---
+++ R/maxAdapterContent.R
@@ -0,0 +1,55 @@
+#' @title Get the maximum Adapter Content
+#'
+#' @description Get the maximum Adapter Content across one or more FASTQC reports
+#'
+#' @details This will extract the \code{Adapter_Content} from the supplied object,
+#' and provide a \code{data_frame} with the final value for each file
+#'
+#' @param x Can be a \code{FastqcFile}, \code{FastqcFileList}, \code{FastqcData},
+#' \code{FastqcDataList} or path
+#' @param digits \code{numeric}.
+#' The output will have the percentages rounded to the specified number of digits
+#' @param asPercent \code{logical}.
+#' Format the values as percentages with the added \code{\%} symbol
+#'
+#' @return A \code{data_frame} object containing the percent of reads with each adapter
+#' type at the final position
+#'
+#' @import tibble
+#' @importFrom dplyr group_by
+#' @importFrom dplyr summarise
+#' @importFrom dplyr mutate
+#' @importFrom reshape2 melt
+#' @importFrom reshape2 dcast
+#' @importFrom magrittr %>%
+#' @importFrom magrittr %<>%
+#' @importFrom scales percent
+#'
+#' @export
+maxAdapterContent <- function(x, digits = 2, asPercent = TRUE){
+
+  # Get the AdapterContent
+  ac <- tryCatch(Adapter_Content(x))
+
+  # Perform the summary
+  ac %<>%
+    reshape2::melt(id.vars = c("Filename", "Position"),
+                   variable.name = "Type")%>%
+    dplyr::group_by(Filename, Type) %>%
+    dplyr::summarise(value = max(value))
+
+  # Format the output
+  if (!is.numeric(digits)) {
+    message("The digits argument was not supplied as a numeric value and will be ignored.")
+  }
+  else {
+    digits <- floor(digits)[1] # Silently ignore any additional values
+    ac %<>% dplyr::mutate(value = round(value, digits))
+  }
+  if (asPercent) ac %<>% dplyr::mutate(value = scales::percent(0.01*value))
+
+  ac %>%
+    reshape2::dcast(Filename~Type, value.var = "value") %>%
+    tibble::as_data_frame()
+
+}
--- R/plotAdapterContent.R
+++ R/plotAdapterContent.R
@@ -7,14 +7,19 @@
 #' with a set of minimal defaults.
 #' The output of this function can be further modified using the standard ggplot2 methods.
 #'
+#' TOset the faceting outside the function, simply set \code{facet = FALSE}
+#' and apply \code{facet_wrap()} after the call to \code{plotAdapterContent}
+#'
 #' @param x Can be a \code{FastqcFile}, \code{FastqcFileList}, \code{FastqcData},
 #' \code{FastqcDataList} or path
 #' @param facet logical. Determines whether to use \code{facet_wrap()} or not
 #' @param nc Number of columns to use when faceting by Adapter Type.
 #' Must be supplied if using facets.
-#' @param ylim A vector providing limits for the y-axis. Defaults to \code{ylim = c(0, 100)}
-#' @param th A ggplot2 theme object. Defaults to \code{theme_bw()}
-#' @param trimNames logical. Remove the file suffix from the legend.
+#' @param ylim A \code{numeric vector} providing limits for the y-axis.
+#' Defaults to \code{ylim = c(0, 100)}
+#' @param th A \code{ggplot2 theme} object. Defaults to \code{theme_bw()}
+#' @param trimNames \code{logical}. Remove the file suffix from the names displyed in the legend.
+#' @param pattern A regular expression used to filter which adapter(s) are plotted
 #' @param ... Not used
 #'
 #' @return A standard ggplot2 object
@@ -22,10 +27,14 @@
 #' @import ggplot2
 #' @importFrom dplyr mutate
 #' @importFrom dplyr select
+#' @importFrom dplyr filter
 #' @importFrom reshape2 melt
 #'
 #' @export
-plotAdapterContent <- function(x, facet = TRUE, nc, ylim, th, trimNames = TRUE, ...){
+plotAdapterContent <- function(x, facet = TRUE, nc, ylim, th, trimNames = TRUE, pattern, ...){
+
+  # Get the AdapterContent
+  ac <- tryCatch(Adapter_Content(x))
 
   if (missing(ylim)) {
     message("The 'ylim' argument was not specified. The default of (0, 100) will be applied.")
@@ -35,9 +44,6 @@ plotAdapterContent <- function(x, facet = TRUE, nc, ylim, th, trimNames = TRUE, 
     if (any(length(ylim) != 2, !is.numeric(ylim))) stop("The argument ylim must be a numeric vector of length 2")
   }
   stopifnot(is.logical(facet))
-
-  # Get the AdapterContent
-  ac <- Adapter_Content(x)
 
   # # Check the alternate names
   # if (!missing(altNames)) {
@@ -66,6 +72,12 @@ plotAdapterContent <- function(x, facet = TRUE, nc, ylim, th, trimNames = TRUE, 
                        value.name = "Percent", variable.name = "Type")
   ac <- mutate(ac, Type = gsub("_", " ", Type))
 
+  # Restrict to a given pattern if requested
+  if (!missing(pattern)) {
+    ac <- dplyr::filter(ac, grepl(pattern, Type))
+    if(nrow(ac) == 0) stop("No adapters matching the supplied pattern were found")
+  }
+
   # Create the basic plot
   acPlot <- ggplot2::ggplot(ac, ggplot2::aes(x = Position, y = Percent, colour = Filename)) +
     ggplot2::geom_line() +
@@ -85,7 +97,7 @@ plotAdapterContent <- function(x, facet = TRUE, nc, ylim, th, trimNames = TRUE, 
     th <- ggplot2::theme_bw()
   }
   else{
-    if (any(!c("theme", "gg") %in% class(th))) {
+    if (!ggplot2::is.theme(th)) {
       warning("Theme supplied as th is not a ggplot theme and will be ignored")
       th <- ggplot2::theme_bw()
     }
@@ -94,4 +106,5 @@ plotAdapterContent <- function(x, facet = TRUE, nc, ylim, th, trimNames = TRUE, 
 
   # And draw the plot
   acPlot
+
 }
