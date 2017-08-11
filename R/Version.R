846523822fc0e60293916280520d90827d912c5a
steveped <stephen.pederson.au@gmail.com>
2017-08-11 15:22:28 +0930

    Removed more errors

---
+++ R/Version.R
@@ -0,0 +1,28 @@
+#' Get the FASTQC version
+#'
+#' @description Get the FASTQC version used to generate the intial files
+#'
+#'
+#' @param object An object of class \code{FastqcData} or \code{FastqcDataList}
+#'
+#' @return A character vector
+#'
+#' @include AllClasses.R
+#' @include AllGenerics.R
+#'
+#' @docType methods
+
+#' @export
+#' @rdname Version
+#' @aliases Version
+setMethod("Version", "FastqcData", function(object){object@Version})
+
+#' @export
+#' @rdname Version
+#' @aliases Version
+setMethod("Version", "FastqcDataList",
+          function(object){
+            data_frame(Filename = fileNames(object),
+                       Version = vapply(object@.Data, Version, character(1)))
+          })
+
