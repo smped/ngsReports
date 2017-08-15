1276122bda3d7ed7c7a4cf4a63c1709141d5bee4
Steve Pederson <stephen.pederson@adelaide.edu.au>
2017-08-15 18:15:12 +0930

    Tidied a few more global variable issues

--- R/importHisat2Logs.R
+++ R/importHisat2Logs.R
@@ -6,10 +6,6 @@
 #'
 #' @return A \code{data_frame}
 #'
-#' @importFrom dplyr select
-#' @importFrom dplyr bind_rows
-#' @importFrom dplyr everything
-#'
 #' @export
 importHisat2Logs <- function(logs){
 
@@ -43,16 +39,16 @@ importHisat2Logs <- function(logs){
         Unique_Discordant_Pairs = as.integer(gsub("([0-9]*) \\(.+\\) aligned discordantly 1 time", "\\1", x[8])),
         Unique_Unpaired = as.integer(gsub("([0-9]*) \\(.+\\) aligned exactly 1 time", "\\1", x[13])),
         Multiple_Unpaired = as.integer(gsub("([0-9]*) \\(.+\\) aligned >1 times", "\\1", x[14])),
-        Not_Aligned = as.integer(gsub("([0-9]*) \\(.+\\) aligned 0 times", "\\1", x[12])),
-        Alignment_Rate = 1 - Not_Aligned / (Total_Reads + Paired_Reads))
+        Not_Aligned = as.integer(gsub("([0-9]*) \\(.+\\) aligned 0 times", "\\1", x[12])))
+      df$Alignment_Rate = with(df, 1 - Not_Aligned / (Total_Reads + Paired_Reads))
     }
     else{
-      df <- dplyr:data_frame(
+      df <- dplyr::data_frame(
         Total_Reads = as.integer(gsub("([0-9]*) reads; of these:", "\\1", x[1])),
         Not_Aligned = as.integer(gsub("([0-9]*) \\(.+\\) aligned 0 times", "\\1", x[3])),
         Unique_Unpaired = as.integer(gsub("([0-9]*) \\(.+\\) aligned exactly 1 time", "\\1", x[4])),
-        Multiple_Unpaired = as.integer(gsub("([0-9]*) \\(.+\\) aligned >1 times", "\\1", x[5])),
-        Alignment_Rate = 1 - Not_Aligned / Total_Reads)
+        Multiple_Unpaired = as.integer(gsub("([0-9]*) \\(.+\\) aligned >1 times", "\\1", x[5])))
+      df$Alignment_Rate = with(df, 1 - Not_Aligned / Total_Reads)
     }
     df
   }
