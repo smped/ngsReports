9c622eaa6f5c0ba0a46db1e0da745577bb7b025a
Chris <a1632244@adelaide.edu.au>
2017-08-22 19:30:54 +0930

    added a stop if you have all 0 values for N content

--- R/plotNContentPlotly.R
+++ R/plotNContentPlotly.R
@@ -8,17 +8,18 @@
 #' May be useful to only return totals from R1 files, or any other subset
 #' @param pwfCols Object of class \code{\link{PwfCols}} to give colours for pass, warning, and fail
 #' values in plot
-#' @param pattern \code{character}.
-#' Contains a regular expression which will be captured from fileName.
-#' The default will capture all text preceding .fastq/fastq.gz/fq/fq.gz
+#' @param labels An optional named factor of labels for the file names.
+#' All filenames must be present in the names.
+#' File extensions are dropped by default.
 #' @param clusterNames \code{logical} default \code{FALSE}. If set to \code{TRUE},
 #' fastqc data will be clustered using heirachial clustering
-#' @param trimNames \code{logical}. Remove the file suffix from the names displyed in the legend.
 #' @param dendrogram \code{logical} redundant if \code{clusterNames} and \code{usePlotly} are \code{FALSE}.
 #' if both \code{clusterNames} and \code{dendrogram} are specified as \code{TRUE} then the dendrogram
 #' will be displayed.
 #' @param usePlotly \code{logical} Default \code{FALSE} will render using ggplot.
 #' If \code{TRUE} plot will be rendered with plotly
+#' @param ... Used to pass various potting parameters to theme.
+#' Can also be used to set size and colour for box outlines.
 #'
 #' @return A ggplot2 or plotly object
 #'
@@ -60,11 +61,11 @@
 plotNContentPlotly <- function(x,
                                subset,
                                pwfCols,
-                               pattern = "(.+)\\.(fastq|fq).*",
+                               labels,
                                clusterNames = FALSE,
-                               trimNames = TRUE,
                                dendrogram = FALSE,
-                               usePlotly = FALSE){
+                               usePlotly = FALSE,
+                               ...){
 
   # A basic cautionary check
   stopifnot(grepl("(Fastqc|character)", class(x)))
@@ -81,12 +82,35 @@ plotNContentPlotly <- function(x,
   x <- tryCatch(x[subset])
   df <- tryCatch(Per_base_N_content(x))
 
-  if (trimNames && stringr::str_detect(pattern, "\\(.+\\)")) {
-    df$Filename <- gsub(pattern[1], "\\1", df$Filename)
-    # These need to be checked to ensure non-duplicated names
-    if (length(unique(df$Filename)) != length(x)) stop("The supplied pattern will result in duplicated filenames, which will not display correctly.")
+  # Drop the suffix, or check the alternate labels
+  if (missing(labels)){
+    labels <- structure(gsub(".(fastq|fq|bam).*", "", fileName(x)),
+                        names = fileName(x))
   }
+  else{
+    if (!all(fileName(x) %in% names(labels))) stop("All file names must be included as names in the vector of labels")
+  }
+  if (length(unique(labels)) != length(labels)) stop("The labels vector cannot contain repeated values")
 
+  # Get any arguments for dotArgs that have been set manually
+  dotArgs <- list(...)
+  if ("size" %in% names(dotArgs)){
+    lineWidth <- dotArgs$size
+  }
+  else{
+    lineWidth <- 0.2
+  }
+  if ("colour" %in% names(dotArgs) || "color" %in% names(dotArgs)){
+    i <- which(names(dotArgs) %in% c("colour", "color"))
+    lineCol <- dotArgs[[i]]
+  }
+  else{
+    lineCol <- "grey20"
+  }
+  allowed <- names(formals(ggplot2::theme))
+  keepArgs <- which(names(dotArgs) %in% allowed)
+  userTheme <- c()
+  if (length(keepArgs) > 0) userTheme <- do.call(theme, dotArgs[keepArgs])
 
   df <- dplyr::rename(df, Percentage = `N-Count`)
   df <- dplyr::mutate(df, Base = factor(Base, levels = unique(Base)))
@@ -98,8 +122,7 @@ plotNContentPlotly <- function(x,
   col <- getColours(pwfCols)
 
   basicStat <- Basic_Statistics(x) %>%
-    dplyr::select(Filename, Longest_sequence) %>%
-    dplyr::mutate(Filename = gsub(pattern[1], "\\1", .$Filename))
+    dplyr::select(Filename, Longest_sequence)
   df <- dplyr::right_join(df, basicStat, by = "Filename")
 
   #split data into correct lengths and fill NA's
@@ -116,10 +139,9 @@ plotNContentPlotly <- function(x,
 
 
   # Convert from wide to long & set the correct variable types
-
+  df <- reshape2::dcast(df, Filename ~ Start)
   #cluster names true hclust names
   if(clusterNames){
-    df <- reshape2::dcast(df, Filename ~ Start)
     xx <- dplyr::select(df, -Filename)
     xx[is.na(xx)] <- 0
     clus <- as.dendrogram(hclust(dist(xx), method = "ward.D2"))
@@ -128,33 +150,40 @@ plotNContentPlotly <- function(x,
     df <- reshape2::melt(df, id.vars = "Filename", variable.name = "Start", value.name = "Percentage")
   }
 
+  key <- df$Filename
+  df <- reshape2::melt(df, id.vars = "Filename", variable.name = "Start", value.name = "Percentage")
+  df$Filename <- labels[df$Filename]
+
+
   df <- dplyr::mutate(df, Percentage = as.numeric(Percentage),
                       Start = as.integer(Start),
                       Filename = factor(Filename, levels = unique(Filename)))
   Nheatmap <- ggplot(df, aes(x = Start, y = Filename, fill = Percentage)) +
-    geom_raster() +
-    scale_fill_gradientn(colours = c(col["PASS"], col["PASS"], col["WARN"], col["WARN"], col["FAIL"], col["FAIL"]),
-                         values = scales::rescale(c(0,5,5,20,20,30)),
-                         guide = "colorbar", limits=c(0, 40),
-                         breaks = c(0, 5, 10, 20, 40)) +
+    geom_tile(colour = lineCol, size = lineWidth) +
+    scale_fill_pwf(df$Percentage, pwfCols, breaks = c(0, 5, 20, 30)) +
     theme(panel.grid.minor = element_blank(),
           panel.background = element_blank())
 
   if(usePlotly){
 
-    Nheatmap <- Nheatmap + theme(axis.text.y = element_blank(),
-                                 axis.ticks.y = element_blank())
+    if(all(df$Percentage == 0))stop("N-content is Zero for all samples and therefore cannot be plot")
+
+    nx <- length(x)
+    Nheatmap <- Nheatmap +
+      geom_hline(yintercept = seq(1.5, nx), colour = lineCol, size = lineWidth) +
+      theme(axis.text.y = element_blank(),
+            axis.ticks.y = element_blank())
 
 
     t <- dplyr::filter(getSummary(x), Category == "Per base N content")
-    t <- dplyr::mutate(t, FilenameFull = Filename,
-                       Filename = gsub(pattern[1], "\\1", t$Filename),
-                       Filename = factor(Filename, levels = unique(df$Filename)))
+    t$Filename <- labels[t$Filename]
+    t <- dplyr::mutate(t, Filename = factor(Filename, levels = unique(df$Filename)))
     t <- dplyr::right_join(t, unique(df["Filename"]), by = "Filename")
-    key <- t$FilenameFull
+
 
     sideBar <- ggplot(t, aes(x = 1, y = Filename, key = key)) +
       geom_tile(aes(fill = Status)) +
+      geom_hline(yintercept = seq(1.5, nx), colour = lineCol, size = lineWidth) +
       scale_fill_manual(values = col) +
       theme(panel.grid.minor = element_blank(),
             panel.background = element_blank(),
@@ -178,11 +207,8 @@ plotNContentPlotly <- function(x,
         scale_y_reverse(expand = c(0, 1)) +
         scale_x_continuous(expand = c(0,1))
 
-      dendro <- plotly::ggplotly(dendro) %>%
-        plotly::layout(margin = list(b = 0, t = 0))
-
       Nheatmap <- plotly::subplot(dendro, sideBar, Nheatmap,
-                                  widths = c(0.2, 0.1,0.7), margin = 0,
+                                  widths = c(0.1, 0.1,0.8), margin = 0,
                                   shareY = TRUE) %>%
         plotly::layout(xaxis3 = list(title = "Sequencing Cycle"))
     }
