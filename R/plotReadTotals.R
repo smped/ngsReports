3c7f50ab6f9f0fde518121705163143f4909e229
Chris <a1632244@adelaide.edu.au>
2017-08-25 18:10:34 +0930

    added duplicated sequences line plot

--- R/plotReadTotals.R
+++ R/plotReadTotals.R
@@ -90,12 +90,7 @@ plotReadTotals <- function(x, subset, millions,
     theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
 
   if(usePlotly){
-    rtPlot <- rtPlot + theme(axis.text = element_blank(),
-                             axis.ticks = element_blank(),
-                             axis.title.y = element_blank()) +
-      labs(fill = "P-value")
-
-    rtPlot <- ggplotly(rtPlot)
+        rtPlot <- ggplotly(rtPlot)
   }
   # Draw the plot
   rtPlot
