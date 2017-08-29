bdd2be3a59045322aa01f7b7fcc96aaf0715d9a0
steveped <stephen.pederson.au@gmail.com>
2017-08-29 16:29:32 +0930

    Fixed NOTES relating to plotGcHeatmap

--- R/plotGcHeatmap.R
+++ R/plotGcHeatmap.R
@@ -51,14 +51,22 @@
 #' plotGcHeatmap(fdl, counts = TRUE)
 #'
 #'
-#' @importFrom dplyr group_by
-#' @importFrom dplyr mutate
-#' @importFrom dplyr ungroup
-#' @importFrom dplyr filter
-#' @importFrom dplyr select
-#' @importFrom dplyr summarise
 #' @importFrom stats as.dendrogram
 #' @importFrom stats order.dendrogram
+#' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 aes_string
+#' @importFrom ggplot2 geom_tile
+#' @importFrom ggplot2 geom_segment
+#' @importFrom ggplot2 theme
+#' @importFrom ggplot2 element_blank
+#' @importFrom ggplot2 scale_fill_gradient2
+#' @importFrom ggplot2 scale_fill_gradientn
+#' @importFrom ggplot2 labs
+#' @importFrom ggplot2 coord_flip
+#' @importFrom ggplot2 scale_x_continuous
+#' @importFrom ggplot2 scale_y_reverse
+#' @importFrom viridisLite inferno
+#' @importFrom magrittr %>%
 #'
 #' @export
 plotGcHeatmap <- function(x, usePlotly = FALSE, counts = FALSE,
@@ -67,6 +75,19 @@ plotGcHeatmap <- function(x, usePlotly = FALSE, counts = FALSE,
                           species = "Hsapiens", GCobject,
                           dendrogram = FALSE, ...){
 
+  df <- tryCatch(Per_sequence_GC_content(x))
+
+  # Drop the suffix, or check the alternate labels
+  if (missing(labels)){
+    labels <- structure(gsub(".(fastq|fq|bam).*", "", fileName(x)),
+                        names = fileName(x))
+  }
+  else{
+    if (!all(fileName(x) %in% names(labels))) stop("All file names must be included as names in the vector of labels")
+  }
+  if (length(unique(labels)) != length(labels)) stop("The labels vector cannot contain repeated values")
+
+  # Tidy up the GC content variables
   if(GCtheory & missing(GCobject)){
     GCobject <- ngsReports::gcTheoretical
   }
@@ -84,21 +105,6 @@ plotGcHeatmap <- function(x, usePlotly = FALSE, counts = FALSE,
       stop(cat("Currently only supports transcriptomes for", spp$Name, sep = ", "))
     }
   }
-
-  if (missing(pwfCols)) pwfCols <- ngsReports::pwf
-  col <- getColours(pwfCols)
-
-  df <- tryCatch(Per_sequence_GC_content(x))
-
-  # Drop the suffix, or check the alternate labels
-  if (missing(labels)){
-    labels <- structure(gsub(".(fastq|fq|bam).*", "", fileName(x)),
-                        names = fileName(x))
-  }
-  else{
-    if (!all(fileName(x) %in% names(labels))) stop("All file names must be included as names in the vector of labels")
-  }
-  if (length(unique(labels)) != length(labels)) stop("The labels vector cannot contain repeated values")
 
   # Get any arguments for dotArgs that have been set manually
   dotArgs <- list(...)
@@ -124,64 +130,62 @@ plotGcHeatmap <- function(x, usePlotly = FALSE, counts = FALSE,
   if (!counts){
 
     # Summarise to frequencies & initialise the plot
-    df <- dplyr::group_by(df, Filename) %>%
-      dplyr::mutate(Freq = Count / sum(Count)) %>%
-      dplyr::ungroup() %>%
-      dplyr::select(Filename, GC_Content, Value = Freq)
+    df <- lapply(split(df, df$Filename), function(x){
+      x$Value <-  x$Count / sum(x$Count)
+      x
+    }) %>%
+      dplyr::bind_rows()
 
     if(GCtheory){
-      df <- df %>%
-        split(.["Filename"]) %>%
-        lapply(function(x){
-          gcTheoryDF <- getGC(GCobject, name = species, type = GCtheoryType)
-          x <- dplyr::mutate(x, Value = Value - unlist(gcTheoryDF[species]))
-        }) %>%
+      df <- lapply(split(df, df$Filename), function(x){
+        gcTheoryDF <- getGC(GCobject, name = species, type = GCtheoryType)
+        x$Value <- x$Value - unlist(gcTheoryDF[species])
+        x
+      }) %>%
         dplyr::bind_rows()
     }
-  }else{
-    df <- dplyr::select(df, Filename, GC_Content, Value = Count)
-  }
-
-  df <- reshape2::dcast(df, Filename ~ GC_Content)
-
-  if(clusterNames){
-    xx <- dplyr::select(df, -Filename)
-    xx[is.na(xx)] <- 0
-    clus <- as.dendrogram(hclust(dist(xx), method = "ward.D2"))
-    row.ord <- order.dendrogram(clus)
-    df <- df[row.ord,]
-  }
-
-  key <- df$Filename
-  df <- reshape2::melt(df, id.vars = "Filename", variable.name = "GC_Content", value.name = "Value")
-  df$Filename <- labels[df$Filename]
-
-  if(!counts){
-    df <- dplyr::mutate(df, Frequency = as.numeric(Value),
-                        GC_Content = as.integer(GC_Content),
-                        Filename = factor(Filename, levels = unique(Filename)))
-    GCheatmap <- ggplot(df, aes(x = GC_Content, y = Filename, fill = Frequency))
+    fillLab <- "Frequency"
   }
   else{
-    df <- dplyr::mutate(df, Counts = as.numeric(Value),
-                        GC_Content = as.integer(GC_Content),
-                        Filename = factor(Filename, levels = unique(Filename)))
-    GCheatmap <- ggplot(df, aes(x = GC_Content, y = Filename, fill = Counts))
+    df$Value <- df$Count
+    fillLab <- "Count"
   }
 
-  GCheatmap <- GCheatmap + geom_tile() +
+  if(clusterNames){
+    # Grab the main columns & cast from long to wide
+    mat <- reshape2::acast(df[c("Filename", "GC_Content", "Value")], Filename ~ GC_Content, value.var = "Value")
+    mat[is.na(mat)] <- 0
+    clus <- as.dendrogram(hclust(dist(mat), method = "ward.D2"))
+    row.ord <- order.dendrogram(clus)
+    mat <- mat[row.ord,]
+    # Reform the data frame from the matrix
+    df <- reshape2::melt(mat, varnames = c("Filename", "GC_Content"), value.name = "Value")
+    df$Filename <- as.character(df$Filename)
+    df$Value <- as.numeric(df$Value)
+    df$GC_Content <- as.integer(df$GC_Content)
+  }
+
+  df$Filename <- labels[df$Filename]
+  df$Filename <- factor(df$Filename, levels = unique(df$Filename))
+
+  # Draw the heatmap
+  GCheatmap <- ggplot(df, aes_string(x = "GC_Content", y = "Filename", fill = "Value")) +
+    geom_tile() +
+    scale_x_continuous(expand = c(0, 0)) +
     theme(panel.grid.minor = element_blank(),
           panel.background = element_blank())
 
   if(GCtheory){
     GCheatmap <- GCheatmap +
+      labs(fill = "Difference from\nTheoretical GC") +
       scale_fill_gradient2(low = inferno(1, begin = 0.4),
                            high = inferno(1, begin = 0.9),
                            midpoint = 0, mid = inferno(1, begin = 0))
   }
   else{
     GCheatmap <- GCheatmap +
-      scale_fill_gradientn(colours = viridisLite::inferno(50))
+      labs(fill = fillLab) +
+      scale_fill_gradientn(colours = inferno(50))
   }
   if (!is.null(userTheme)) GCheatmap <- GCheatmap + userTheme
 
@@ -191,13 +195,18 @@ plotGcHeatmap <- function(x, usePlotly = FALSE, counts = FALSE,
       theme(axis.text.y = element_blank(),
             axis.ticks.y = element_blank())
 
-    t <- getSummary(x) %>% dplyr::filter(Category == "Per sequence GC content")
-    t$Filename <- labels[t$Filename]
-    t$Filename <- factor(t$Filename, levels = unique(df$Filename))
+    t <- getSummary(x)
+    t <- t[t$Category == "Per sequence GC content",]
+    t$Filename <- factor(labels[t$Filename], levels = unique(df$Filename))
     t <- dplyr::right_join(t, unique(df["Filename"]), by = "Filename")
+    t$x <- 1
+    t$key <- labels[as.character(t$Filename)]
 
-    sideBar <- ggplot(t, aes(x = 1, y = Filename, key = key)) +
-      geom_tile(aes(fill = Status)) +
+    if (missing(pwfCols)) pwfCols <- ngsReports::pwf
+    col <- getColours(pwfCols)
+
+    sideBar <- ggplot(t, aes_string(x = "x", y = "Filename", key = "key")) +
+      geom_tile(aes_string(fill = "Status")) +
       scale_fill_manual(values = col) +
       theme(panel.grid.minor = element_blank(),
             panel.background = element_blank(),
@@ -205,13 +214,13 @@ plotGcHeatmap <- function(x, usePlotly = FALSE, counts = FALSE,
             axis.title=element_blank(),
             axis.text=element_blank(),
             axis.ticks=element_blank())
-    sideBar <- plotly::ggplotly(sideBar, tooltip = c("Status", "Filename"))
+    sideBar <- suppressMessages(plotly::ggplotly(sideBar, tooltip = c("Status", "Filename")))
 
     #plot dendrogram
     if(dendrogram){
       ggdend <- function(df) {
         ggplot() +
-          geom_segment(data = df, aes(x=x, y=y, xend=xend, yend=yend)) +
+          geom_segment(data = df, aes_string("x","y", xend = "xend", yend = "yend")) +
           ggdendro::theme_dendro()
       }
 
@@ -227,10 +236,15 @@ plotGcHeatmap <- function(x, usePlotly = FALSE, counts = FALSE,
         plotly::layout(xaxis3 = list(title = "GC Content (%)"))
     }
     else{
-      GCheatmap <- plotly::subplot(plotly_empty(), sideBar, GCheatmap, widths = c(0.1,0.1,0.8), margin = 0, shareY = TRUE) %>%
-        plotly::layout(xaxis3 = list(title = "GC Content (%)"),
-                       annotations = list(text = "Filename", showarrow = FALSE,
-                                          textangle = -90))
+      GCheatmap <- suppressWarnings(
+        suppressMessages(
+          plotly::subplot(plotly::plotly_empty(), sideBar, GCheatmap,
+                          widths = c(0.1,0.1,0.8), margin = 0, shareY = TRUE) %>%
+            plotly::layout(xaxis3 = list(title = "GC Content (%)"),
+                           annotations = list(text = "Filename", showarrow = FALSE,
+                                              textangle = -90))
+        )
+      )
     }
 
   }
