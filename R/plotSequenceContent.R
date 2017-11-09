6f1c065b2fd559082f39e289acd39cec84221697
Steve Pederson <stephen.pederson@adelaide.edu.au>
2017-11-09 11:20:19 +1030

    Hid another "use devtools to install ggplot2" message

--- R/plotSequenceContent.R
+++ R/plotSequenceContent.R
@@ -66,65 +66,66 @@ plotSequenceContent <- function(x, usePlotly = FALSE, labels, plotType = "heatma
 
   if(plotType == "heatmap"){
 
-  maxBase <- max(vapply(c("A", "C", "G", "T"), function(x){max(df[[x]])}, numeric(1)))
-  df$colour <- with(df, rgb(floor(`T`) / maxBase, floor(A) / maxBase, floor(C) / maxBase, 1 - floor(G) / maxBase))
+    maxBase <- max(vapply(c("A", "C", "G", "T"), function(x){max(df[[x]])}, numeric(1)))
+    df$colour <- with(df, rgb(floor(`T`) / maxBase, floor(A) / maxBase, floor(C) / maxBase, 1 - floor(G) / maxBase))
 
-  basicStat <- Basic_Statistics(x)[c("Filename", "Longest_sequence")]
+    basicStat <- Basic_Statistics(x)[c("Filename", "Longest_sequence")]
 
-  df <- dplyr::right_join(df, basicStat, by = "Filename")
-  df <- df[c("Filename", "Start", "colour", "Longest_sequence", "A", "C", "G", "T")]
+    df <- dplyr::right_join(df, basicStat, by = "Filename")
+    df <- df[c("Filename", "Start", "colour", "Longest_sequence", "A", "C", "G", "T")]
 
-  df <- split(df, f = df$Filename) %>%
-    lapply(function(x){
-      dfFill <- data.frame(Start = 1:x[["Longest_sequence"]][1])
-      x <- dplyr::right_join(x, dfFill, by = "Start") %>%
-        zoo::na.locf()
-    }) %>%
-    dplyr::bind_rows()
-  df$Start <- as.integer(df$Start)
-  key <- unique(df$Filename)
-  df$Filename <- labels[df$Filename]
-  df$Filename <- factor(df$Filename, levels = unique(df$Filename))
-  tileCols <- unique(df$colour)
-  names(tileCols) <- unique(df$colour)
+    df <- split(df, f = df$Filename) %>%
+      lapply(function(x){
+        dfFill <- data.frame(Start = 1:x[["Longest_sequence"]][1])
+        x <- dplyr::right_join(x, dfFill, by = "Start") %>%
+          zoo::na.locf()
+      }) %>%
+      dplyr::bind_rows()
+    df$Start <- as.integer(df$Start)
+    key <- unique(df$Filename)
+    df$Filename <- labels[df$Filename]
+    df$Filename <- factor(df$Filename, levels = unique(df$Filename))
+    tileCols <- unique(df$colour)
+    names(tileCols) <- unique(df$colour)
 
-  sequenceContentHeatmap <- ggplot(df,
-                                   aes_string(x = "Start", y = "Filename",
-                                       fill = "colour", A = "A", C = "C", G = "G", T = "T")) +
-    geom_tile() +
-    scale_fill_manual(values = tileCols) +
-    scale_x_continuous(expand = c(0, 0)) +
-    scale_y_discrete(expand = c(0, 0)) +
-    theme(legend.position = "none",
-          panel.grid.minor = element_blank(),
-          panel.grid.major = element_blank(),
-          panel.background = element_rect(fill = "black")) +
-    labs(x = "Position in read (bp)",
-         y = "Filename")
+    sequenceContentHeatmap <- ggplot(df,
+                                     aes_string(x = "Start", y = "Filename",
+                                                fill = "colour", A = "A", C = "C", G = "G", T = "T")) +
+      geom_tile() +
+      scale_fill_manual(values = tileCols) +
+      scale_x_continuous(expand = c(0, 0)) +
+      scale_y_discrete(expand = c(0, 0)) +
+      theme(legend.position = "none",
+            panel.grid.minor = element_blank(),
+            panel.grid.major = element_blank(),
+            panel.background = element_rect(fill = "black")) +
+      labs(x = "Position in read (bp)",
+           y = "Filename")
 
-  if (usePlotly){
-    sequenceContentHeatmap <- sequenceContentHeatmap +
-      theme(axis.ticks.y = element_blank(),
-            axis.text.y = element_blank())
-
-
-    t <- getSummary(x)
-    t <- t[t$Category == "Per base sequence content",]
-    t$Filename <- labels[t$Filename]
-    t$Filename <- factor(t$Filename, levels = levels(df$Filename))
+    if (usePlotly){
+      sequenceContentHeatmap <- sequenceContentHeatmap +
+        theme(axis.ticks.y = element_blank(),
+              axis.text.y = element_blank())
 
 
-    sideBar <- makeSidebar(status = t, key = key, pwfCols = pwfCols)
+      t <- getSummary(x)
+      t <- t[t$Category == "Per base sequence content",]
+      t$Filename <- labels[t$Filename]
+      t$Filename <- factor(t$Filename, levels = levels(df$Filename))
 
-    subplot(sideBar, sequenceContentHeatmap, widths = c(0.08,0.92), margin = 0.001) %>%
-      layout(xaxis2 = list(title = "Position in read (bp)"))
+      sideBar <- makeSidebar(status = t, key = key, pwfCols = pwfCols)
+
+      suppressMessages( # Hides the recommendation to install from github...
+        subplot(sideBar, sequenceContentHeatmap, widths = c(0.08,0.92), margin = 0.001) %>%
+        layout(xaxis2 = list(title = "Position in read (bp)"))
+      )
 
 
-
-      }
-  else{
-    sequenceContentHeatmap
-  }}
+    }
+    else{
+      sequenceContentHeatmap
+    }
+  }
   else{
     df$Filename <- labels[df$Filename]
     df <- df[!colnames(df) == "Base"]
@@ -146,8 +147,11 @@ plotSequenceContent <- function(x, usePlotly = FALSE, labels, plotType = "heatma
       theme_bw() +
       scale_colour_manual(values = baseCols)
     if(usePlotly){
-      ggplotly(sequenceContentHeatmap) %>% layout(legend = list(x = 0.85, y = 1))
-    }else{
+      suppressMessages(
+        ggplotly(sequenceContentHeatmap) %>% layout(legend = list(x = 0.85, y = 1))
+      )
+    }
+    else{
       sequenceContentHeatmap
     }
   }
