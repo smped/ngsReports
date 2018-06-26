056588ff61e3b4caf76991c0b7b8b442402de51e
Steve Pederson <stephen.pederson@adelaide.edu.au>
2018-06-27 00:36:23 +0930

    Update to include checks

--- R/writeHtmlReport.R
+++ R/writeHtmlReport.R
@@ -6,6 +6,8 @@
 #' @param template The template file which will be copied into \code{fastqcDir}
 #' @param species Species/closely related species of sequenced samples
 #' @param dataType Is the data "Transcriptomic" or "Genomic" in nature?
+#' @param nOver The maximum number of Overrepresented Sequences to show
+#' @param nKmer The maximum number of Kmers to show
 #' @param targetsDF A data.frame with at least two columns named \code{Filename} and \code{Label}.
 #' The filenames should match the original fastq files, and the labels should be simply alternative
 #' labels for these files for convenience.
@@ -18,7 +20,8 @@
 #'
 #'
 #' @export
-writeHtmlReport <- function(fastqcDir, template, species = "Hsapiens", dataType = "Transcriptome",
+writeHtmlReport <- function(fastqcDir, template, species = "Hsapiens", 
+                            dataType = "Transcriptome", nOver = 30, nKmer = 30,
                             targetsDF, overwrite = FALSE, quiet = TRUE){
 
   # Include checks for the package webshot & PhantomJS
@@ -51,6 +54,15 @@ writeHtmlReport <- function(fastqcDir, template, species = "Hsapiens", dataType 
       message("Invalid targetsDF. No file will be exported.")
     }
   }
+  
+  # Check the remaining arguments
+  dataType <- match.arg(dataType, c("Transcriptome", "Genome"))
+  nOver <- suppressWarnings(as.integer(nOver[1]))
+  nKmer<- suppressWarnings(as.integer(nKmer[1]))
+  stopifnot(!is.na(c(nOver, nKmer)))
+  gcFun <- tryCatch(match.arg(tolower(dataType), c("genomes","transcriptomes")))
+  avail <- do.call(gcFun, list(object = gcTheoretical))
+  species <- tryCatch(match.arg(species, avail$Name))
 
   # Compile the document in the directory
   htmlOut <- file.path(fastqcDir, gsub(".Rmd$", ".html", basename(template)))
@@ -58,7 +70,8 @@ writeHtmlReport <- function(fastqcDir, template, species = "Hsapiens", dataType 
   rmarkdown::render(file2Knit, output_format = "html_document",
                     output_file = basename(htmlOut), output_dir = dirname(file2Knit),
                     envir = new.env(), quiet = quiet,
-                    params = list(dataType = dataType, species = species))
+                    params = list(dataType = dataType, species = species,
+                                  nOver = nOver, nKmer = nKmer))
   message("done")
 
   # Return the message, along with invisible(TRUE/FALSE)
