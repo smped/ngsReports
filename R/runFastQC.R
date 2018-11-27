1a4f4a5be88e5ff7deb8673364f88ca34885762f
Steve Ped <stephen.pederson@adelaide.edu.au>
2018-11-28 01:01:29 +1100

    Implemented `runFastQC()` as an S4 method

--- R/runFastQC.R
+++ R/runFastQC.R
@@ -2,20 +2,25 @@
 #'
 #' @description Takes a FastqFileList
 #'
-#' @details This is a simple wrapper function for controlling & running \code{fastqc} from within R.
-#' This can be very useful for controlling & documenting an entire pipeline from within knitr to produce a simple report
+#' @details This is a simple wrapper function for controlling & running 
+#' \code{fastqc} from within R.
+#' This can be very useful for controlling & documenting an entire pipeline from 
+#' within knitr to produce a simple report
 #'
 #' Only the common functinoality of FastQC is implemented,
 #' for more fine detail pease call FastQC directly.
 #'
 #' @param object A \code{\link{FastqFileList}}
-#' @param outPath The path to write the FastQC reports. Must exist as it will not be created when calling this function
+#' @param outPath The path to write the FastQC reports. Must exist as (for safety)
+#' it will not be created when calling this function
 #' @param exec The location of the fastqc executable.
 #' @param threads The number of threads to run in parallel
 #' @param casava logical. Sets the \code{--casava} flag
 #' @param nofilter logical. Sets the \code{--nofilter} flag
 #' @param extract logical. Extract the zip files on completion of the report
 #' @param nogroup logical. Sets the grouping of bases for reads longer than 50bp
+#' @param min_length integer. Sets an artificial lower limit on the length of the 
+#' sequence to be shown in the report.  
 #' @param contaminants Path to an alternate file with contaminants.
 #' The structure of the file will not be checked.
 #' Refer to the \code{fastqc} help page for more details
@@ -23,31 +28,96 @@
 #' The structure of the file will not be checked.
 #' Refer to the \code{fastqc} help page for more details
 #' @param kmers An integer between 2 and 10
-#' @param overWrite logical. Overwrite any existing reports
 #'
 #' @return An object of class \code{FastqcFileList}
 #'
 #' @author Steve Pederson <stephen.pederson@@adelaide.edu.au>
 #' @seealso \code{\link{FastqFileList}}
+#' 
+#' @examples 
+#' \dontrun{
+#' library(ShortRead)
+#' sp <- SolexaPath(system.file('extdata', package='ShortRead'))
+#' fl <- file.path(analysisPath(sp), "s_1_sequence.txt")
+#' f <- FastqFile(fl)
+#' fqcFile <- runFastQC(f, outPath = tempdir())
+#' }
 #'
 #' @importFrom parallel detectCores
-#' @importFrom methods is
 #' @importClassesFrom ShortRead FastqFile FastqFileList
+#' @importClassesFrom Rsamtools BamFile BamFileList
 #'
 #' @export
-runFastQC <- function(object, outPath, exec = "/usr/local/bin/fastqc",
-                      threads=1L, casava = FALSE, nofilter = FALSE, extract = FALSE,
-                      nogroup = FALSE, contaminants = c(), adapters = c(), kmers = 7,
-                      overWrite = FALSE){
+#' @rdname runFastQC-methods
+setGeneric("runFastQC", function(object, outPath, exec = "/usr/local/bin/fastqc",
+                                 threads=1L, casava = FALSE, nofilter = FALSE, 
+                                 extract = FALSE, nogroup = FALSE, min_length = 1,
+                                 contaminants = c(), adapters = c(), kmers = 7){
+    standardGeneric("runFastQC")
+})
+#' @aliases runFastQC,FastqFile-method
+#' @rdname runFastQC-methods
+#' @export
+setMethod("runFastQC", "FastqFile",
+          function(object, outPath, exec = "/usr/local/bin/fastqc",
+                   threads=1L, casava = FALSE, nofilter = FALSE, extract = FALSE,
+                   nogroup = FALSE, min_length = 1, contaminants = c(), 
+                   adapters = c(), kmers = 7){
+              
+              runFastQC.default(object, outPath, exec, threads, casava, nofilter,
+                                extract, nogroup, min_length, contaminants, 
+                                adapters, kmers, fileType = "FastqFile") 
+          }
+)
+#' @aliases runFastQC,FastqFileList-method
+#' @rdname runFastQC-methods
+#' @export
+setMethod("runFastQC", "FastqFileList",
+          function(object, outPath, exec = "/usr/local/bin/fastqc",
+                   threads=1L, casava = FALSE, nofilter = FALSE, extract = FALSE,
+                   nogroup = FALSE, min_length = 1, contaminants = c(), 
+                   adapters = c(), kmers = 7){
+        
+              runFastQC.default(object, outPath, exec, threads, casava, nofilter,
+                                extract, nogroup, min_length, contaminants, 
+                                adapters, kmers, fileType = "FastqFileList")      
+              
+          }
+)
+#' @aliases runFastQC,BamFile-method
+#' @rdname runFastQC-methods
+#' @export
+setMethod("runFastQC", "BamFile",
+          function(object, outPath, exec = "/usr/local/bin/fastqc",
+                   threads=1L, casava = FALSE, nofilter = FALSE, extract = FALSE,
+                   nogroup = FALSE, min_length = 1, contaminants = c(), 
+                   adapters = c(), kmers = 7){
+              
+              runFastQC.default(object, outPath, exec, threads, casava, nofilter,
+                                extract, nogroup, min_length, contaminants, 
+                                adapters, kmers, fileType = "BamFile") 
+          }
+)
+#' @aliases runFastQC,BamFileList-method
+#' @rdname runFastQC-methods
+#' @export
+setMethod("runFastQC", "BamFileList",
+          function(object, outPath, exec = "/usr/local/bin/fastqc",
+                   threads=1L, casava = FALSE, nofilter = FALSE, extract = FALSE,
+                   nogroup = FALSE, min_length = 1, contaminants = c(), 
+                   adapters = c(), kmers = 7){
+              
+              runFastQC.default(object, outPath, exec, threads, casava, nofilter,
+                                extract, nogroup, min_length, contaminants, 
+                                adapters, kmers, fileType = "BamFileList") 
+          }
+)
+
+runFastQC.default <- function(object, outPath, exec, threads, casava, nofilter, 
+                              extract, nogroup, min_length, contaminants, 
+                              adapters, kmers, fileType){
     
-    # Check the FastqFileList
-    stopifnot(is(object,"FastqFileList"))
-    nm <- names(object)
-    if (any(!grepl("(fastq|fastq.gz|fq|fq.gz)$", BiocGenerics::path(object)))) stop("Files can only contain the fasqt|fq suffix")
-    # Make sure they are all of the same format
-    suffix <- gsub(".+(fastq|fastq.gz|fq|fq.gz)$", "\\1", nm)
-    suffix <- paste0(".", unique(suffix))
-    stopifnot(length(suffix) == 1)
+    stopifnot(!missing(fileType))
     
     # Check the output path exists
     stopifnot(file.exists(outPath))
@@ -58,32 +128,31 @@ runFastQC <- function(object, outPath, exec = "/usr/local/bin/fastqc",
     
     # Check the executable exists
     stopifnot(file.exists(exec))
-    
-    # Set the expected output names
-    fqcNames <- file.path(outPath, gsub(suffix, "_fastqc", nm))
-    if (!extract) fqcNames <- paste0(fqcNames, ".zip")
-    if (!overWrite && all(file.exists(fqcNames))) {
-        message("All reports exist and were not overwritten")
-        return(FastqcFileList(fqcNames))
+    # Get the version
+    v <- system2(exec, "-v", stdout = TRUE)
+    v <- gsub(".+0.11.(.+)", "\\1", v)
+    if (as.integer(v) > 5){
+        min_length <- paste("--min_length", min_length)
+    }
+    else{
+        message("min_length cannot be set for FastQC < 0.11.6")
+        min_length <- c()
     }
     
-    # It might be worth addiing a section here to not overwrite any existing files,
-    # and to run fastQC on the remaining files
-    # The entire set would need to be kept for the final output though
-    
     # Set the arguments to the function call
-    maxCores <- parallel::detectCores()
+    maxCores <- parallel::detectCores() - 1
+    if (maxCores < threads) message("Too many threads. Resetting to", maxCores)
     threads <- paste("-t", min(threads, maxCores))
     stopifnot(is.logical(casava), is.logical(nogroup))
     if (casava){
-        casava <- dplyr::if_else(nofilter, "--casava --nofilter", "--casava")
+        casava <- ifelse(nofilter, "--casava --nofilter", "--casava")
     }
     else {
         casava <- c()
     }
     extract <- dplyr::if_else(extract, "--extract", "--noextract")
     if (nogroup) {
-        warning("Setting the option '--nogroup' may cause FastQC to become unstable")
+        message("Setting the option '--nogroup' may cause FastQC to become unstable")
         nogroup <- "--nogroup"
     }
     else{
@@ -101,7 +170,8 @@ runFastQC <- function(object, outPath, exec = "/usr/local/bin/fastqc",
     stopifnot(!is.na(kmers), kmers %in% 2:10)
     kmers <- paste("-k", kmers)
     
-    args <- paste("-o", outPath, threads, casava, extract, nogroup, contaminants, adapters, kmers)
+    args <- paste("-o", outPath, threads, casava, extract, nogroup, 
+                  contaminants, adapters, kmers)
     args <- gsub(" +", " ", args) #Remove any double spaces
     files <- paste(BiocGenerics::path(object), collapse = " ")
     
@@ -109,14 +179,33 @@ runFastQC <- function(object, outPath, exec = "/usr/local/bin/fastqc",
     message("Executing the command '", paste(exec, args), "'")
     system2(exec, paste(args, files))
     
-    # Check that everything was written as expected
-    if (any(!file.exists(fqcNames))) warning("Reports not written for:\n", fqcNames[!file.exists(fqcNames)])
+    # Get the files and make sure there are no html files returned
+    fqcNames <- list.files(outPath, pattern = "fastqc", 
+                           full.names = TRUE)
+    fqcNames <- grep(fqcNames, 
+                     pattern = "html", invert = TRUE, value = TRUE)
     
-    # Get the version number for the output
-    v <- system2(exec, "-v", stdout = TRUE)
     
-    # Return a FastqcFileList, until extracting the summary has been incorporated into the
-    # getFastqcData methods for a FastqcFileList
-    FastqcFileList(fqcNames)
+    # Now define the format as required
+    if (fileType %in% c("FastqFileList", "BamFileList")){
+        # Return a FastqcFileList
+        ffl <- FastqcFileList(fqcNames)
+        # Now return the files that have been run in the same order
+        m <- pmatch(gsub("(.+)\\..+", "\\1", names(object)), fileName(ffl))
+        if (anyNA(m)) {
+            warning("NA values indicate Fastqc files may be missing for one or more samples")
+            m <- m[!is.na(m)]
+        }
+        out <- ffl[m]    
+    }
+    if (fileType %in% c("FastqFile", "BamFile")){
+        # Return a FastqcFileList
+        ffl <- FastqcFileList(fqcNames)
+        # Now return the file that has been run 
+        nm <- basename(path(object))
+        m <- pmatch(gsub("(.+)\\..+", "\\1", nm), fileName(ffl))
+        out <- ffl[[m]]    
+    }
     
+    out
 }
