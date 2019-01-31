978c7534566e21108eecdaefd4737bcf9086f39b
Steve Ped <stephen.pederson@adelaide.edu.au>
2019-02-01 02:11:06 +1030

    Revised vignette

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' @title Theoretical GC content
 #'
-#' @description This object contains the theoretical GC content for each 
+#' @description This object contains the theoretical GC content for each
 #' provided species, for both the genome and transcriptome, where available.
 #'
 #' @details The object is defined with the S4 class \code{TheoreticalGC}.
@@ -10,7 +10,19 @@
 #'
 #' Metadata is accessible using \code{mData(gcTheoretical)}.
 #'
-#' All GC content was calculated using code modified from 
-#' https://github.com/mikelove/fastqcTheoreticalGC
+#' All GC content was calculated using code from
+#' https://github.com/mikelove/fastqcTheoreticalGC using BSgenome packages.
+#' This provides a default set of GC content data for common organisms
+#' generated using 100bp reads/fragments and 1e6 reads.
+#'
+#' @examples
+#' ## Check which genomes are included
+#' genomes(gcTheoretical)
+#'
+#' ## Check which transcriptomes are included
+#' transcriptomes(gcTheoretical)
+#'
+#' ## Metadata is also included
+#' mData(gcTheoretical)
 #'
 "gcTheoretical"
